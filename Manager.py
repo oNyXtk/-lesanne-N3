e2f3d4d1c3617a13a090d46db684d5ab8fb1a717
oNyXtk <assasin.outlast@gmail.com>
2025-05-21 09:50:49 +0300

    Add files via upload

---
+++ Manager.py
@@ -0,0 +1,6 @@
+Manager = MurderStatsManager()
+
+manager.add_data("Europe", "Finland", "Helsinki" 20)
+manager.add_data("America", "NewYork", "WashingtonDC" 50)
+manager.add_data("Asia", "Japan", "Tokyo", 100)
+
--- MurderStatsManager.py
+++ MurderStatsManager.py
@@ -1,30 +1,22 @@
 class MurderStatManager:
-     def add_data(self, continent, country, city, count):
-         self.continent = continent
-         self.country = country
-         self.city = city
-         self.count = count
-
-class Europe:
-     def __init__(self, name, city, count):
-       self.name = Finland
-       self.city = Helsinki
-       self.count = 30000
-     print("This country name is")
-
-class America:
-    def __init__(self, name, city, count):
-        self.name = NewYork
-        self.city = WashingtonDC
-        self.count = 50000
-    
-
-class Asia:
-    def __init__(self,name,city,count):
-        self.name = Japan
-        self.city = Tokyo
-        self.count = 40000
-    print("")
-
+  def __init__(self):
+      self.data = {}
+      
+def add_data(self, continent, country, city, count):
+        if continent not in self.data:
+            self.data[continent] = {}
+        if country not in self.data[continent]:
+            self.data[continent][country] = {}
+            self.data[continent][country][city] = count
+        
+        
 def get_stats(self):
-    print("")
+    return self.data
+        
+
+        
+        
+
+              
+              
+          
