524eff6f8ef193cc0e2c76b22bd4c8276d41ef89
oNyXtk <assasin.outlast@gmail.com>
2025-05-21 10:33:55 +0300

    Delete Manager.py

--- Manager.py
+++
@@ -1,6 +0,0 @@
-Manager = MurderStatsManager()
-
-manager.add_data("Europe", "Finland", "Helsinki" 20)
-manager.add_data("America", "NewYork", "WashingtonDC" 50)
-manager.add_data("Asia", "Japan", "Tokyo", 100)
-
