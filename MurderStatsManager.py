58d91c86b486341e6cb184570812989e7418effe
oNyXtk <assasin.outlast@gmail.com>
2025-05-21 10:27:07 +0300

    Add files via upload

--- MurderStatsManager.py
+++ MurderStatsManager.py
@@ -1,17 +1,35 @@
-class MurderStatManager:
-  def __init__(self):
-      self.data = {}
-      
-def add_data(self, continent, country, city, count):
-        if continent not in self.data:
-            self.data[continent] = {}
-        if country not in self.data[continent]:
-            self.data[continent][country] = {}
-            self.data[continent][country][city] = count
-        
-        
-def get_stats(self):
-    return self.data
+class MurderStatsManager():
+    def __init__(self):
+        self.data = {}
+    def add_data(self, continent, country, city, count):
+        for path, value in self.data.items():
+            if path == continent:        
+                for path2 , value2 in value.items():
+                    if path2 == country: 
+                        for path3 , value3 in value2.items():
+                            if path3 == city:
+                                value2[path3] = count  
+                                return
+    def get_stats(self):
+        for path, value in self.data.items():
+            print(path)
+            print(value)
+stats = MurderStatsManager()
+stats.data = {
+    "Europe": {
+        "Estonia": {
+            "Tallinn": 5
+        },
+        "Finland": {
+            "Helsinki": 3
+        }
+    },
+    "Asia": {
+        "Japan": {
+            "Tokyo": 8
+        }
+    }
+}
         
 
         
